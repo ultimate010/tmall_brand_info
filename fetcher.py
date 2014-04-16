a13b7f3ded1bb972d857b0b978e87bf50d0665e8
ultimate010 <ultimate010@gmail.com>
2014-04-16 17:55:12 +0800

    Add *.py

---
+++ fetcher.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python
+#-*-coding:utf-8-*-
+import httplib2
+from threading import Thread,Lock
+from Queue import Queue
+import time
+import datetime
+from gzip import GzipFile
+import socket
+import sys
+
+socket.setdefaulttimeout(3) #设置5秒后连接超时
+
+class fetcher:
+    def __init__(self,threads):
+        self.lock = Lock() #线程锁
+        self.q_req = Queue() #任务队列
+        self.q_ans = Queue() #完成队列
+        self.threads = threads
+        self.running = 0
+        for i in range(threads):
+            t = Thread(target=self.threadget,args=(i , ))
+            t.setDaemon(True)
+            t.start()
+
+    def __del__(self): #解构时需等待两个队列完成
+        time.sleep(0.5)
+        self.q_req.join()
+        self.q_ans.join()
+
+    def taskleft(self):
+        sys.stderr.write("req:%d\tans:%d\trunning:%d\n" % (self.q_req.qsize(),self.q_ans.qsize(),self.running))
+        #return self.q_req.qsize()+self.q_ans.qsize()+self.running
+
+    def push(self,req):
+        self.q_req.put(req)
+
+    def pop(self):
+        data = self.q_ans.get()
+        self.q_ans.task_done() #ans finish
+        return data
+    def haveResult(self):
+        return self.q_ans.qsize()
+
+    def threadget(self,myId):
+        opener = httplib2.Http(".cache",timeout = 3) #复用
+        while True:
+            req = self.q_req.get()
+            self.lock.acquire() #要保证该操作的原子性，进入critical area
+            #sys.stderr.write("myId:%d\tEnter lock to get req\n"%(myId))
+            self.running += 1
+            self.lock.release()
+            #sys.stderr.write("myId:%d\tOut lock to get req\n"%(myId))
+#            try:
+            ans = self.get(myId,req,opener)
+            if ans == '':
+                sys.stderr.write("myId:%d\t%s:no ans\n"%(myId,req))
+            else:
+                sys.stderr.write("myId:%d\t%s:Get ans\n"%(myId,req))
+#            except Exception, what:
+#                sys.stderr.write("myId:%d\t%s:%s\n" % (myId,datetime.datetime.now(),what))
+            self.q_ans.put((req,ans))
+            self.lock.acquire() #要保证该操作的原子性，进入critical area
+            #sys.stderr.write("myId:%d\tEnter lock to put ans\n"%(myId))
+            self.running -= 1
+            self.lock.release()
+            self.q_req.task_done() #req finish
+            #sys.stderr.write("myId:%d\tOut lock to put ans\n"%(myId))
+            time.sleep(5)
+
+    def get(self,myId,req,opener,retries=3):
+        try:
+            headers_template = {
+                            'Accept': 'application/xml,application/xhtml+xml,text/html;q=0.9,text/plain;q=0.8,image/png,*/*;q=0.5',
+                            'Accept-Charset': 'UTF-8,*;q=0.5',
+                            'Accept-Encoding': 'gzip,deflate,sdch',
+                            'Accept-Language': 'zh-CN,zh;q=0.8',
+                            'Cache-Control': 'no-cache',
+                            'Connection': 'keep-alive',
+                            'Host': 'brand.tmall.com',
+                            'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/534.24 (KHTML, like Gecko) Chrome/11.0.696.65 Safari/534.24',
+            }
+            (resp_headers , data) = opener.request(req , "GET" , headers = headers_template ,  redirections = 10)
+#            print data
+        except Exception , what:
+            if retries>0:
+                time.sleep(3)
+                return self.get(myId,req,opener,retries-1)
+            else:
+                sys.stderr.write("myId:%d:\t%s:GET FAIL\tREQ:%s\t%s\n"% (myId,datetime.datetime.now(),req,what))
+                return ''
+        return data
+
+if __name__ == "__main__":
+    links = [ 'http://www.verycd.com/topics/%d/'%i for i in range(5420,5430) ]
+    f = fetcher(threads=5)
+    count = 0
+    for url in links:
+        f.push(url)
+        count += 1
+    while count > 0:
+
+        f.taskleft()
+        time.sleep(1)
+        url,content = f.pop()
+        count -= 1
+        print url,len(content)
---
+++ taobao_cat.py
@@ -0,0 +1,134 @@
+#!/usr/bin/env python
+#-*-coding:utf-8-*-
+import time
+import datetime
+from fetcher import fetcher
+import sys
+import lxml.html as H
+import re
+from pyhelper import pyhelper_add_helper, pyhelper_main
+
+threadsNum = 4
+
+LEIMU_XPATH = '//div[@class="catFilter"]/dl/dd/a/@href'
+BRAND_LOGO_XPATH = '//div[@class="brandItem"]/div[@class="brandItem-info"]/div/p/a/img/@data-ks-lazyload'
+BRAND_URL_XPATH = '//div[@class="brandItem"]/div[@class="brandItem-info"]/div/p/a/@href'
+BRAND_TITLE_XPATH = '//div[@class="brandItem"]/div[@class="brandItem-info"]/div/p/strong/text()'
+BRAND_DESC_XPATH = '//div[@class="brandItem"]/div[@class="brandItem-info"]/div/dl/dd/text()'
+PATTERN_BRANDID = 'brandId=([0-9]+)'
+PATTERN_YE = u'共([0-9]+)页'
+PATTERN_INDID = "\?industryId=([0-9]+)&"
+PATTERN_CATID = "&categoryId=([0-9]+)&"
+PATTERN_ETID = "\&etgId=([0-9]+)"
+
+def genLevelFirst(fet,urls):
+    try:
+        result = []
+        taskCount = 0
+        for url in urls:
+            taskCount += 1
+            fet.push(url)
+        while taskCount > 0:
+            taskCount -= 1
+            url , content = fet.pop()
+            fet.taskleft() #watch ans
+            dom_lei = H.fromstring(content)
+            classUrls = dom_lei.xpath(LEIMU_XPATH)
+            if ( len(classUrls) == 0):
+                sys.stderr.write("%s:Can not paser %s\n" % (datetime.datetime.now(),url))
+            for u in classUrls:
+                #raw_input()
+                #print u
+                result.append(u)
+        return result
+    except Exception as err:
+        sys.stderr.write("%s:%s\n" % (datetime.datetime.now(),err))
+
+
+
+
+def main():
+    if len(sys.argv) != 2:
+        print "Usage: python %s firstLevelurl  1> outfile 2>logFile" % sys.argv[0]
+        exit(1)
+    f = fetcher(threads=threadsNum)
+    with open(sys.argv[1],'r') as fi:
+        urls = fi.readlines()
+        urls = map (lambda x: x.strip("\r\n") , urls)
+        level1Urls = genLevelFirst(f,urls)
+    for url in level1Urls:
+        print url
+
+@pyhelper_add_helper
+def genLevelSecond(secondLevelurlsfile,outfile,thirdfile):
+    f = fetcher(threads=threadsNum)
+    with open(secondLevelurlsfile , 'r') as fi:
+        urls = fi.readlines()
+        urls = map (lambda x: x.strip("\r\n") , urls)
+        ans = _genLevelSecond(f,urls,thirdfile)
+    fout = open(outfile , 'w')
+    for  url in ans:
+        fout.write("%s\n" % url.encode('utf-8' , 'ignore'))
+def _genLevelSecond(fet , urls,thirdfile):
+    try:
+        fthree = open(thirdfile , 'w')
+        pye = re.compile(PATTERN_YE , re.M)
+        pindustryid = re.compile(PATTERN_INDID , re.M)
+        pcategoryid = re.compile(PATTERN_CATID , re.M)
+        petgid = re.compile(PATTERN_ETID , re.M)
+        pbrandid = re.compile(PATTERN_BRANDID , re.M)
+        taskCount = 0
+        for url in urls:
+            taskCount += 1
+            fet.push(url)
+        while taskCount > 0:
+            taskCount -= 1
+            url , content = fet.pop()
+            content = content.decode('gbk','ignore')
+            did = pindustryid.findall(url)
+            cid = pcategoryid.findall(url)
+            eid = petgid.findall(url)
+            #print content
+            ans = pye.findall(content)
+    #        print did[0]
+    #        print cid[0]
+    #        print eid[0]
+            if(len(ans) > 0):
+                fthree.write("%s\t%s\n" % (url , ans[0]))
+            else:
+                fthree.write("%s\tERRO\n" % (url))
+
+            fet.taskleft() #watch ans
+#            print content
+            dom_lei = H.fromstring(content)
+
+            logourls = dom_lei.xpath(BRAND_LOGO_XPATH)
+            brandurls = dom_lei.xpath(BRAND_URL_XPATH)
+            titles = dom_lei.xpath(BRAND_TITLE_XPATH)
+            dess = dom_lei.xpath(BRAND_DESC_XPATH)
+#            print brandurls[0]
+#            print titles[0]
+#            print dess[0]
+#            print logourls[0]
+#            raw_input()
+            if (len(brandurls) == 0):
+                sys.stderr.write("%s:Can not paser %s\n" % (datetime.datetime.now(),url))
+            str = ''
+            try:
+                for i , _url  in enumerate(brandurls):
+                    bid = pbrandid.findall(_url)
+                    str = titles[i] + "\t" + dess[i] + "\t" + bid[0] + "\t" + cid[0] + "\t" + _url + "\t" + logourls[i]
+#                   raw_input()
+                    yield str
+            except Exception as err:
+                fthree.write("%s\tERRO\n" % (url))
+                sys.stderr.write("%s:%s\t%s\n" % (datetime.datetime.now(),url , err))
+
+    except Exception as err:
+        sys.stderr.write("%s:%s\n" % (datetime.datetime.now(),err))
+
+
+
+if __name__ == '__main__':
+    pyhelper_main()
+    #main()
